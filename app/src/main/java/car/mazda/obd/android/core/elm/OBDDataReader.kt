d8c023022c2bd4ec9e66ade1c5b8b1f5e1b5e2b5
Aleksandr Shchekotov <alexandrschekotov931201@gmail.com>
2026-06-26 16:56:28 +0300

    Revert "Make OBD polling less aggressive"
    This reverts commit 9a2bfe7ba53c08e042216a1feebcfbd3879af0d1.

--- app/src/main/java/car/mazda/obd/android/core/elm/OBDDataReader.kt
+++ app/src/main/java/car/mazda/obd/android/core/elm/OBDDataReader.kt
@@ -2,9 +2,6 @@ package car.mazda.obd.android.core.elm
 
 import car.mazda.obd.android.core.elm.entity.OBDRequest
 import car.mazda.obd.android.core.elm.entity.OBDResponse
-import car.mazda.obd.android.core.elm.exception.AdapterUnreachableException
-import car.mazda.obd.android.core.elm.exception.LostConnectionException
-import car.mazda.obd.android.core.elm.exception.NetworkUnavailableException
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
@@ -21,13 +18,6 @@ class OBDDataReader(
     private val client: OBDClient,
     private val sessionManager: OBDSessionManager,
 ) {
-    private companion object {
-        private const val ADAPTER_TIMEOUT_RECONNECT_THRESHOLD = 3
-    }
-
-    private val reconnectStateLock = Any()
-    private var consecutiveAdapterTimeouts = 0
-
     fun rpmFlow(periodMs: Long): Flow<OBDResponse> =
         requestFlow(periodMs, OBDRequest.EngineRpm)
 
@@ -46,33 +36,11 @@ class OBDDataReader(
 
     private suspend fun safeRequest(request: OBDRequest): OBDResponse {
         return try {
-            client.requestObd(request).also { resetReconnectState() }
+            client.requestObd(request)
         } catch (t: Throwable) {
             if (t is CancellationException) throw t
-            if (shouldReconnect(t)) {
-                sessionManager.requestReconnect(t)
-            }
+            sessionManager.requestReconnect(t)
             OBDResponse.NoData.Error("", t)
-        }
-    }
-
-    private fun shouldReconnect(t: Throwable): Boolean =
-        when (t) {
-            is AdapterUnreachableException -> registerAdapterTimeout()
-            is LostConnectionException,
-            is NetworkUnavailableException -> true
-            else -> false
-        }
-
-    private fun registerAdapterTimeout(): Boolean =
-        synchronized(reconnectStateLock) {
-            consecutiveAdapterTimeouts += 1
-            consecutiveAdapterTimeouts >= ADAPTER_TIMEOUT_RECONNECT_THRESHOLD
-        }
-
-    private fun resetReconnectState() {
-        synchronized(reconnectStateLock) {
-            consecutiveAdapterTimeouts = 0
         }
     }
 
